425319af3fe5c465b676967a49932d8ed8454c75
Laetitia Commanay <lmc2266@columbia.edu>
2023-01-14 17:35:42 +0100

    added controller actions for licorne

--- app/controllers/licornes_controller.rb
+++ app/controllers/licornes_controller.rb
@@ -1,2 +1,28 @@
 class LicornesController < ApplicationController
+  def index
+    @licornes = Licornes.all
+  end
+
+  def show
+    @licorne = Licorne.find(params[:id])
+  end
+
+  def new
+    @licorne = Licorne.new
+  end
+
+  def create
+    @licorne = Licorne.new(licorne_params)
+    if @licorne.save
+      redirect_to licorne_path(@licorne)
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def licorme_params
+    params.require(:licorne).permit(:price, :name, :age, :superpower)
+  end
 end
