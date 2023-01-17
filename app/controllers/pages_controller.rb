9825ea56cb7ca5f6700c2ae28cd2b5d33b41583e
Raphaelle PEREZ HORNBORG <raphaelleph@gmail.com>
2023-01-17 20:48:34 +0100

    controller pages dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,10 @@
 class PagesController < ApplicationController
   def home
   end
+
+  def dashboard
+    @booking_requests = Booking.where(booking.licorne.user == current_user && booking.status == "pending")
+    @confirmed_bookings = Booking.where(booking.licorne.user == current_user && booking.status == "confirmed")
+    @my_booking_requests = Booking.where(booking.user == current_user)
+  end
 end
