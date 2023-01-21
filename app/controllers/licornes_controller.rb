fce8cca0e776d04a4ac9d0485b2deca898e9c31f
Raphaelle PEREZ HORNBORG <raphaelleph@gmail.com>
2023-01-21 11:22:44 +0100

    authentication OK

--- app/controllers/licornes_controller.rb
+++ app/controllers/licornes_controller.rb
@@ -1,5 +1,5 @@
 class LicornesController < ApplicationController
-  #skip_before_action :authenticate_user!, only: %i[index show]
+  skip_before_action :authenticate_user!, only: %i[index show]
 
   def index
     @licornes = Licorne.all
