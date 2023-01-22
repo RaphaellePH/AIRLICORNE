486f23faa93421f90bc2f9b7b2f264996a22930d
Adrien De Tchaguine <adriendetchaguine@gmail.com>
2023-01-22 18:47:58 +0100

    adding geocoder gem

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_21_154304) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_22_162611) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -75,6 +75,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_21_154304) do
     t.string "first_name"
     t.string "last_name"
     t.string "address"
+    t.float "latitude"
+    t.float "longitude"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
